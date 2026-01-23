b3b3e34bfeb41fe4cb7b62c8fe47de3664873ddd
Adam Evans <46998078+Riokajin@users.noreply.github.com>
2026-01-23 14:40:34 -0500

    finished lab 2

--- 03/03_make_it_maintainable/index.php
+++ 03/03_make_it_maintainable/index.php
@@ -5,30 +5,10 @@
     - Repetition will become a problem
 
     How can we refactor this code so it’s easier to maintain?
+
+    reflection: I learned from this lab that it can be quite easy to keep things separated so that they can always be added exactly how you need them to pages that are generated or changed based on user input. It will greatly reduce the amount of work needed to get everything to mesh well later on with this course.
 */
 
-$items = ["Home", "About", "Contact"];
-
-?>
-
-<!DOCTYPE html>
-<html>
-<head>
-    <title>My PHP Page</title>
-</head>
-<body>
-
-<h1>Welcome</h1>
-
-<ul>
-<?php foreach ($items as $item): ?>
-    <li><?= $item ?></li>
-<?php endforeach; ?>
-</ul>
-
-<footer>
-    <p>&copy; 2026</p>
-</footer>
-
-</body>
-</html>
+require "header.php";
+require "nav.php";
+require "footer.php";
---
+++ 03/03_make_it_maintainable/nav.php
@@ -0,0 +1,8 @@
+<?php
+$items = ["Home", "About", "Contact"];
+?>
+
+<ul><?php foreach ($items as $item): ?>
+    <li><?=  $item ?></li>
+<?php endforeach; ?>
+</ul>
