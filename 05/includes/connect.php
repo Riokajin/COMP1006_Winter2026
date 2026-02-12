801d762cb8d7733361874d8b6f63f73d563abf24
Adam Evans <46998078+Riokajin@users.noreply.github.com>
2026-02-12 15:33:53 -0500

    week 5 stuff

--- 05/includes/connect.php
+++ 05/includes/connect.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 $host = "localhost"; //hostname
 $db = "bitumi"; //database name
 $user = "root"; //username
@@ -9,10 +9,10 @@ $dsn = "mysql:host=$host;dbname=$db";
 
 //try to connect, if connected echo a yay!
 try {
-   $pdo = new PDO ($dsn, $user, $password); 
+   $pdo = new PDO ($dsn, $user, $password);
    $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 }
-//what happens if there is an error connecting 
+//what happens if there is an error connecting
 catch(PDOException $e) {
-    die("Database connection failed: " . $e->getMessage()); 
+    die("Database connection failed: " . $e->getMessage());
 }
--- 05/process.php
+++ 05/process.php
@@ -68,6 +68,27 @@ if (!empty($errors)) {
 
 /*4*/
 
+$sql = "INSERT INTO orders (first_name, last_name, phone, address, email, comments) VALUES (:first_name, :last_name, :phone, :address, :email, :comments)";
+//prepare the query
+
+$stmt = $pdo->prepare($sql);
+
+//map the named placeholder to the user data/actual data
+
+$stmt->bindParam(":first_name", $firstName);
+$stmt->bindParam(":last_name", $lastName);
+$stmt->bindParam(":phone", $phone);
+$stmt->bindParam(":address", $address);
+$stmt->bindParam(":email", $email);
+$stmt->bindParam(":comments", $comments);
+
+//execute the query
+$stmt->execute();
+
+$pdo = null; //close the connection
+
+
+
 
 ?>
 <? require "includes/header.php"; ?> 
